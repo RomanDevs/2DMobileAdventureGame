528b7651c70316b161a454ddb2adbbcf6b2b3696
unknown <romangregori@hotmail.com>
2021-03-02 23:29:13 -0300

    merge with dev branch

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerAnimation _pAScript;
+    private Rigidbody2D _rb;
+    private float _speed = 3f;
+    private Vector2 _velocity;
+    private float _jumpForce = 5;
+    private bool _resetJump = false;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        if(_rb == null)
+        {
+            Debug.LogError("Rigid Body 2D is NULL");
+        }
+        _pAScript = GetComponent<PlayerAnimation>();
+        if(_pAScript == null)
+        {
+            Debug.LogError("Player Animation Script is NULL");
+        }
+    }
+
+    void Update()
+    {
+        Movement();
+        
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        _pAScript.Move(horizontal);
+        _velocity = new Vector2(horizontal * _speed, _rb.velocity.y);
+        _pAScript.Jump(CheckGrounded());
+
+        if (Input.GetKeyDown(KeyCode.Space) && CheckGrounded() == true)
+        {
+            _velocity.y = _jumpForce;
+            StartCoroutine(JumpReset());
+        }
+
+
+        _rb.velocity = _velocity;
+    }
+
+    private bool CheckGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x - 0.07f, transform.position.y - 0.71f), Vector2.down, 0.1f, 00001000);
+        Debug.DrawRay(new Vector2(transform.position.x - 0.07f, transform.position.y - 0.71f), Vector2.down * 0.1f, Color.magenta);
+
+        if (hit.collider != null)
+        {
+            if (_resetJump == false)
+            {
+                return true;
+            }
+        }
+            return false;
+    }
+
+    private IEnumerator JumpReset()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+        StopCoroutine(JumpReset());
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private SpriteRenderer _playerSprite;
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        if(_anim == null)
+        {
+            Debug.LogError("Animator is NULL");
+        }
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        if (_playerSprite == null)
+        {
+            Debug.LogError("Sprite Renderer is NULL");
+        }    
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Jump(bool TorF)
+    {
+        _anim.SetBool("Jump", !TorF);
+    }
+    public void Move(float horizontal)
+    {
+        _anim.SetFloat("Speed", Mathf.Abs(horizontal));
+        FaceDirection(horizontal);
+    }
+
+    private void FaceDirection(float horizontal)
+    {
+        if(horizontal > 0)
+        {
+            _playerSprite.flipX = false;
+        }
+
+        if(horizontal < 0)
+        {
+            _playerSprite.flipX = true;
+        }
+    }
+}

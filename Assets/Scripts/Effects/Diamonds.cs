f1d40c1abed3b8bf1a6dfeccdeba65441bd38c8b
unknown <romangregori@hotmail.com>
2021-03-05 18:00:17 -0300

    Diamonds fundamental Setup

---
+++ Assets/Scripts/Effects/Diamonds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamonds : MonoBehaviour
+{
+    [SerializeField] private int _diamondsToGive;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<Player>().AddDiamonds(_diamondsToGive);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IDamagable
 {
+    private int _diamonds;
+
     private PlayerAnimation _pAScript;
     private Rigidbody2D _rb;
     private float _speed = 3f;
@@ -90,6 +92,12 @@ public class Player : MonoBehaviour, IDamagable
         {
             _pAScript.TriggerAttack();
         }
+    }
+
+    public void AddDiamonds(int diamonds)
+    {
+        _diamonds += diamonds;
+        Debug.Log(_diamonds);
     }
 
 }
